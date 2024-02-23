d87f3bb66ab022bc750e77e70d2f78ee5d21335e
maryvito <maryv2@yandex.ru>
2024-02-23 18:48:13 +0300

    init

---
+++ src/test/java/create/topic/KafkaAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package create.topic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
